a08551c1c00b2d836b2d33c5d84fb797756e7b7a
Kennady Ang <kennady@betawerkz.com.sg>
2022-06-20 15:58:13 +0800

    added all stocks ML model and change time return format

--- app.py
+++ app.py
@@ -14,6 +14,29 @@ def lambda_handler(event, context):
     ticker = body_dict["ticker_symbol"]
     data = body_dict["stock_metadata_list"]
 
+    #list of trained models
+    model1D_list = ['AAPL_1D.sav', 'AMZN_1D.sav', 'AMCR_1D.sav', 'TSLA_1D.sav']
+    model7D_list = ['AAPL_7D.sav', 'AMZN_7D.sav', 'AMCR_7D.sav', 'TSLA_7D.sav']
+    model28D_list = ['AAPL_28D.sav', 'AMZN_28D.sav', 'AMCR_28D.sav', 'TSLA_28D.sav']
+
+    #directory in current folder storing trained models
+    dir = 'model/'
+
+    #index of stocks
+    symbol = {
+      "AAPL": 0,
+      "AMZN": 1,
+      "AMCR": 2,
+      "TSLA": 3
+    }
+
+    ticker_index = symbol[ticker]
+
+    #load the various models
+    model_1D = joblib.load(dir + model1D_list[ticker_index])
+    model_7D = joblib.load(dir + model7D_list[ticker_index])
+    model_28D = joblib.load(dir + model28D_list[ticker_index])
+
     Date = []
     Open = []
     High = []
@@ -39,28 +62,23 @@ def lambda_handler(event, context):
 
     df = pd.DataFrame(dataTable)
 
-    #load the various models
-    model_1D = joblib.load('model1D.sav')
-    model_7D = joblib.load('model7D.sav')
-    model_28D = joblib.load('model28D.sav')
-
     #prediction
     result1D = model_1D.predict(df)
     result7D = model_7D.predict(df)
     result28D = model_28D.predict(df)
 
     #extract prediction based on latest date
-    output1D = result1D[0]
-    output7D = result7D[0]
-    output28D = result28D[0]
+    output1D = result1D[-1]
+    output7D = result7D[-1]
+    output28D = result28D[-1]
 
-    #calculate datetime based on request datetime
-    latestDate = Date[0]
+    #format datetime for output
+    latestDate = Date[-1]
     dt = datetime.strptime(latestDate, '%Y-%m-%dT%H:%M:%S.%fZ')
     epochdt = dt.timestamp()
-    x1D = epochdt + 86400
-    x7D = epochdt + 7*86400
-    x28D = epochdt + 28*86400
+    x1D = int(epochdt + 86400 + 28800) * 1000
+    x7D = int(epochdt + 7*86400 + 28800) * 1000
+    x28D = int(epochdt + 28*86400 + 28800) * 1000
 
     #JSON string to return
     ret = json.dumps({
